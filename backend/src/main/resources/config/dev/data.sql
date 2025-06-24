0d604a18a0ce08a1f538b12b5625f0be53da9fe0
saccolinux <93777997+vincenzosacco@users.noreply.github.com>
2025-06-24 05:53:44 +0200

    FIX-CONFIG: refactor database in dev mode (h2 DB) schema and seed data to mock the real postgres db.

--- backend/src/main/resources/config/dev/data.sql
+++ backend/src/main/resources/config/dev/data.sql
@@ -1,11 +1,33 @@
 -- data.sql - Used to populate the h2 (in-memory) database with some test data.
 -- All objects will be deleted when the application is closed or restarted.
-INSERT INTO annuncio (title, description, price)
+
+-- RUOLO DATA ----------
+INSERT INTO ruolo (tipo) VALUES ('ADMIN');
+INSERT INTO ruolo (tipo) VALUES ('BUYER');
+INSERT INTO ruolo (tipo) VALUES ('SELLER');
+
+-- CATEGORIA IMMOBILE DATA ----------
+INSERT INTO categoria_immobile (tipo) VALUES ('Appartamento');
+INSERT INTO categoria_immobile (tipo) VALUES ('Villa');
+INSERT INTO categoria_immobile (tipo) VALUES ('Ufficio');
+-- UTENTE DATA ----------
+INSERT INTO utente (nome, email, "password", ruolo_id, bannato)
+VALUES ('Admin User', 'admin@gmail.com', 'admin123', 1, false);
+INSERT INTO utente (nome, email, "password", ruolo_id, bannato)
+VALUES ('Buyer User', 'buyer@gmail.com', 'buyer123', 2, false);
+INSERT INTO utente (nome, email, "password", ruolo_id, bannato)
+VALUES ('Seller User', 'seller@gmail.com', 'seller123', 3, false);
+-- ANNUNCIO DATA ----------
+
+INSERT INTO annuncio (titolo, descrizione, prezzo, prezzo_asta, metri_quadri, indirizzo, in_vendita, categoria_id,
+                      venditore_id, foto, promozione)
 VALUES ('First Annuncio',
      'This is the first annuncio',
-    100.00);
-
-INSERT INTO annuncio (title, description, price)
+    100000.55,     95000.00, 300, 'Via Roma 1, Milano', true, 1,
+    1, NULL, false);
+INSERT INTO annuncio (titolo, descrizione, prezzo, prezzo_asta, metri_quadri, indirizzo, in_vendita, categoria_id,
+                      venditore_id, data_creazione, foto, promozione)
 VALUES ('Second Annuncio',
-    'This is the second annuncio',
-     200.00);
+     'This is the second annuncio',
+    200000.00,     NULL, 150, 'Via Milano 2, Roma', true, 2,
+    2, CURRENT_TIMESTAMP, NULL, true);
