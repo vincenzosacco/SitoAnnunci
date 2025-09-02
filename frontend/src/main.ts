83ceed5da8d7032c3c4eb1cafeff7989100a05cb
saccolinux <93777997+vincenzosacco@users.noreply.github.com>
2025-09-02 22:58:09 +0200

    ADD: enable component input binding in router configuration

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,12 +1,12 @@
 import {bootstrapApplication} from '@angular/platform-browser';
 import {provideHttpClient, withInterceptors} from '@angular/common/http';
-import {provideRouter} from '@angular/router';
+import {provideRouter, withComponentInputBinding} from '@angular/router';
 import {AppComponent} from './app/app.component';
 import {routes} from './app/app.routes';
 // oAuth protocol provided by https://auth0.com/.
 // https://auth0.com/docs/quickstart/spa/angular/01-login#configure-auth0
 import {provideAuth0} from '@auth0/auth0-angular';
-import { authInterceptor } from './app/services/auth/auth-facade.service';
+import {authInterceptor} from './app/services/auth/auth-facade.service';
 
 bootstrapApplication(AppComponent, {
     providers: [
@@ -14,7 +14,7 @@ bootstrapApplication(AppComponent, {
             // Interceptor to automatically set the Authorization token when making HTTP requests.
             withInterceptors([authInterceptor])
         ),
-        provideRouter(routes),
+        provideRouter(routes, withComponentInputBinding()),
 
         provideAuth0({
             domain: 'dev-t258cewzxm8xksdu.us.auth0.com',
@@ -23,7 +23,7 @@ bootstrapApplication(AppComponent, {
                 redirect_uri: window.location.origin,
                 audience: 'http://localhost:8080',
                 // The scope defines the permissions that the application is requesting.
-                scope: 'openid profile email admin:access read:users'
+                scope: 'openid profile email admin:access read:users',
             }
         })
     ]
