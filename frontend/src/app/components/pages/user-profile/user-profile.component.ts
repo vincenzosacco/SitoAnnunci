1f78e4bf2e2969275ad2ae17a6742daf7680943c
saccolinux <93777997+vincenzosacco@users.noreply.github.com>
2025-09-03 14:58:00 +0200

    UPDATE user-profile.component to show user details

--- frontend/src/app/components/pages/user-profile/user-profile.component.ts
+++ frontend/src/app/components/pages/user-profile/user-profile.component.ts
@@ -1,15 +1,17 @@
 import {Component, inject} from '@angular/core';
-import {User} from '@auth0/auth0-angular';
 import {AuthFacadeService} from "../../../services/auth/auth-facade.service";
+import {AsyncPipe} from "@angular/common";
 
 @Component({
     selector: 'app-user-profile',
     templateUrl: './user-profile.component.html',
-    imports: [],
+    imports: [
+        AsyncPipe
+    ],
     standalone: true
 })
+
 export class UserProfileComponent {
-    protected user: User | null | undefined;
-    protected auth = inject(AuthFacadeService);
+    protected user$ =  inject(AuthFacadeService).user$;
 }
 
