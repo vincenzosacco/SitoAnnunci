ba88849359fb0ccb1b9307c3a06f2920c7adf687
frai1313 <frai@1313>
2025-09-08 01:13:24 +0200

    Feature: asta frontend

--- backend/src/main/java/org/unical/backend/controller/UtentiController.java
+++ backend/src/main/java/org/unical/backend/controller/UtentiController.java
@@ -1,10 +1,7 @@
 package org.unical.backend.controller;
 
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.unical.backend.model.Utente;
 import org.unical.backend.service.IUtenteService;
 
